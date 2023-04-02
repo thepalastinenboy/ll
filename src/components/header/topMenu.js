1b22008ffbf2d61795c83c81b8051fa8812bee4b
ubai97 <ob.mtl97@gmail.com>
2023-04-02 15:47:18 +0200

    init

---
+++ src/components/header/topMenu.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import {CgMenuRight} from "react-icons/cg";
+import {TbLetterW} from "react-icons/tb";
+import { GoHome} from "react-icons/go";
+
+
+
+export const TopMenu = () => {
+    return(
+      <div className="toolbar toolbar-bottom tabbar-labels">
+      <div className="toolbar-inner">
+        <Link className="link tab-link-active" to="/">
+        <GoHome size="1.5em" />
+          
+        </Link>
+        <Link className="link" to="/wortschatz">
+       <TbLetterW size="1.5em" />
+        </Link>
+        <Link className="link" to="/navigation">
+        <CgMenuRight size="1.5em" /> übersicht
+          
+        </Link>
+      </div>
+    </div>
+    
+    )
+}
